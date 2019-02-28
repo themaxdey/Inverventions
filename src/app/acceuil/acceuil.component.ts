6b7b8bc4a9e01a351dd571bc536412efced383de
themaxdey <39136244+themaxdey@users.noreply.github.com>
2019-02-28 18:17:06 -0500

    ok progression

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Inter-acceuil',
+  templateUrl: './acceuil.component.html',
+  styleUrls: ['./acceuil.component.css']
+})
+export class AcceuilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    ProblemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
