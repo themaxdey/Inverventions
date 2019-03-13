8bae3902f9c68511c767b657bac0a88e53b61a0f
themaxdey <39136244+themaxdey@users.noreply.github.com>
2019-03-13 15:09:18 -0400

    test 3 fini

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -39,5 +39,12 @@ describe('ProblemeComponent', () => {
     expect(zone.valid).toBeTruthy();
   });
 
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(200))
+    expect(zone.valid).toBeTruthy();
+  });
+  
+
 
 });
