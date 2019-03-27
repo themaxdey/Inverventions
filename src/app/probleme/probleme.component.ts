472fa39428982344f7840c1a8017a4891cb08245
themaxdey <39136244+themaxdey@users.noreply.github.com>
2019-03-27 15:10:10 -0400

    fini le tp9

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ZonesValidator } from '../shared/longeur-minimum/longeur-minimum.component';
 
 @Component({
   selector: 'Inter-probleme',
@@ -13,7 +14,7 @@ export class ProblemeComponent implements OnInit {
 
   ngOnInit() {
     this.problemeForm = this.fb.group({
-      prenom: ['',[Validators.minLength(3), Validators.required]],
+      prenom: ['',[ZonesValidator.plage(3), Validators.required]],
       
     });
   }
